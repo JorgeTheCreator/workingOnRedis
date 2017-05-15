481a4d804c69e5301d7ad6899502a7bf6b0ae6c5
Jorge Peguero <JrgPeguero@yahoo.com>
2017-05-14 20:57:04 -0400

    first push

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const redis = require('redis');
+
+const app = express();
+// create client
+const client = redis.createClient();
+
+client.on("connect", ()=> {
+    console.log("redis server connected");
+});
+
+
+//view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/',(req,res)=>{
+  let title = 'Task List';
+  client.lrange('tasks', 0, -1, (err, reply)=>{
+      res.render('index',{
+        title: title,
+        tasks: reply
+      });
+  });
+});
+
+app.post('/task/add', (req, res) => {
+   let task = req.body.task;
+
+   client.rpush('tasks', task, (err, reply) => {
+     if(err){
+       console.log(err);
+     }
+       console.log('task added .....');
+       res.redirect('/')
+   })
+})
+
+app.listen(3000,()=>{
+  console.log('Server started at 3000');
+})
+module.exports = app;
